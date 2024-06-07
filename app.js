cebdaff2d561140a23282eb019c15a9206e24f4b
Developer Asad <asaduzzamanasad9005@gmail.com>
2024-06-06 23:58:52 -1200

    remove product

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 document.getElementById("submit").addEventListener("click", function () {
     const date = document.getElementById("date").value;
-    let firstPrizecount = document.getElementById('firstPrize').value;
+    // let firstPrizecount = document.getElementById('firstPrize').value;
     let secondPrizecount = document.getElementById('secondPrize').value;
     let thirdPrizecount = document.getElementById('thirdPrize').value;
     let fourthPrizecount = document.getElementById('fourthPrize').value;
     let fivePrizecount = document.getElementById('fivePrize').value;
     let sixPrizecount = document.getElementById('sixPrize').value;
     let sevenPrizePrizecount = document.getElementById('sevenPrize').value;
-    let eightPrizecount = document.getElementById('eightPrize').value;
+    // let eightPrizecount = document.getElementById('eightPrize').value;
     // let ninePrizecount = document.getElementById('ninePrize').value;
     // let tenPrizecount = document.getElementById('tenPrize').value;
     // let elevenPrizecount = document.getElementById('elevenPrize').value;
 
  
     
-    let firstPrize = parseFloat(firstPrizecount) + 1;
+    // let firstPrize = parseFloat(firstPrizecount) + 1;
 
     let secondPrize = parseFloat(secondPrizecount) + 1;
     let thirdPrize = parseFloat(thirdPrizecount) + 1;
@@ -22,20 +22,20 @@ document.getElementById("submit").addEventListener("click", function () {
     let fivePrize = parseFloat(fivePrizecount) + 1;
     let sixPrize = parseFloat(sixPrizecount) + 1;
     let sevenPrize = parseFloat(sevenPrizePrizecount) + 1;
-    let eightPrize = parseFloat(eightPrizecount) + 1;
+    // let eightPrize = parseFloat(eightPrizecount) + 1;
     // let ninePrize = parseFloat(ninePrizecount) + 1;
     // let tenPrize = parseFloat(tenPrizecount) + 1;
     // let elevenPrize = parseFloat(elevenPrizecount) + 1;
 
 
-    document.getElementById('firstPrize').value = parseFloat(firstPrize.toFixed(2));
+    // document.getElementById('firstPrize').value = parseFloat(firstPrize.toFixed(2));
     document.getElementById('secondPrize').value = parseFloat(secondPrize.toFixed(2));
     document.getElementById('thirdPrize').value = parseFloat(thirdPrize.toFixed(2));
     document.getElementById('fourthPrize').value = parseFloat(fourthPrize.toFixed(2));
     document.getElementById('fivePrize').value = parseFloat(fivePrize.toFixed(2));
     document.getElementById('sixPrize').value = parseFloat(sixPrize.toFixed(2));
     document.getElementById('sevenPrize').value = parseFloat(sevenPrize.toFixed(2));
-    document.getElementById('eightPrize').value = parseFloat(eightPrize.toFixed(2));
+    // document.getElementById('eightPrize').value = parseFloat(eightPrize.toFixed(2));
     // document.getElementById('ninePrize').value = parseFloat(ninePrize.toFixed(2));
     // document.getElementById('tenPrize').value = parseFloat(tenPrize.toFixed(2));
     // document.getElementById('elevenPrize').value = parseFloat(elevenPrize.toFixed(2));
@@ -43,7 +43,7 @@ document.getElementById("submit").addEventListener("click", function () {
     
     
     
-    const subtotal = firstPrize + secondPrize + thirdPrize + fourthPrize + fivePrize + sixPrize + sevenPrize + eightPrize ;
+    const subtotal =   secondPrize + thirdPrize + fourthPrize + fivePrize + sixPrize + sevenPrize;
 
     let subTotalResult = parseFloat(subtotal.toFixed(2));
 
@@ -55,13 +55,13 @@ document.getElementById("submit").addEventListener("click", function () {
   
 
     document.getElementById('secoundPrizeChange').innerText = secondPrize;
-    document.getElementById("firstPrizeChange").innerText = firstPrize;
+    // document.getElementById("firstPrizeChange").innerText = firstPrize;
     document.getElementById("thirdPrizeChange").innerText = thirdPrize;
     document.getElementById("fourthPrizeChange").innerText = fourthPrize;
     document.getElementById("fivePrizeChange").innerText = fivePrize;
     document.getElementById("sixPrizeChange").innerText = sixPrize;
     document.getElementById("sevenPrizeChange").innerText = sevenPrize;
-    document.getElementById("eightPrizeChange").innerText = eightPrize;
+    // document.getElementById("eightPrizeChange").innerText = eightPrize;
     // document.getElementById("ninePrizeChange").innerText = ninePrize;
     // document.getElementById("tentPrizeChange").innerText = tenPrize;
     // document.getElementById("elevenPrizeChange").innerText = elevenPrize;
